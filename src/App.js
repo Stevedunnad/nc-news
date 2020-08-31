a8da76e37a754c0fcd3bdf11eb7378ee032fc23b
“Steve <stevedunnad@gmail.com>
2020-08-31 10:56:11 +0100

    made a basic scaffold of everything

--- src/App.js
+++ src/App.js
@@ -5,7 +5,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>nc news</h1>
+      <nav>
+        <div>Home</div>
+        <div>Articles</div>
+        <div>Topics</div>
+      </nav>
+      <main>
+        <div>
+          <h2>Sort by</h2>
+          <div>Date</div>
+          <div>Author</div>
+          <div>Topic</div>
+          <div>Vote</div>
+        </div>
+        <article>
+          <h3>Title</h3>
+          <p>Body</p>
+        </article>
+      </main>
     </div>
   );
 }
