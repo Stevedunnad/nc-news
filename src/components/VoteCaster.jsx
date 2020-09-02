c339499c6bce8d6bd61f134cfb78dc9861c0c67d
“Steve <stevedunnad@gmail.com>
2020-09-02 13:15:12 +0100

    added click event handler and function to increment votes

--- src/components/VoteCaster.jsx
+++ src/components/VoteCaster.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import * as api from '../utils/api'
 
 const VoteCaster = ({article_id, votes}) => {
+
+  const incrementVote = (e) => {
+    api.patchArticleVotes(article_id, 1)
+  }
+
   return (
     <section>
-      <button>yes!</button>
+      <button onClick={incrementVote}>yes!</button>
       {votes}
       <button>no!</button>
     </section>
