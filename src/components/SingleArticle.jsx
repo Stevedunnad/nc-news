eb5bb37fedc33c002d02563a856822b06e81af59
“Steve <stevedunnad@gmail.com>
2020-09-02 09:22:26 +0100

    created single article component

---
+++ src/components/SingleArticle.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SingleArticle extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Single article</h1>
+      </div>
+    )
+  }
+}
