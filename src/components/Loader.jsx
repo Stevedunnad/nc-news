6f71ff84943e7cf5e26456ecdbabadeaa3fa7936
“Steve <stevedunnad@gmail.com>
2020-09-02 11:33:08 +0100

    loader component created

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div>
+      <h3>Article loading...</h3>
+    </div>
+  );
+};
+
+export default Loader;
--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import * as api from '../utils/api'
+import Loader from './Loader'
 
 export default class SingleArticle extends Component {
 
@@ -9,7 +10,7 @@ export default class SingleArticle extends Component {
   }
 
   componentDidMount() {
-    api.getSingleArticle()
+    api.getSingleArticle(this.props.article_id)
     .then(article => {
       this.setState({article, isLoading: false})
     })
@@ -17,6 +18,8 @@ export default class SingleArticle extends Component {
 
   render() {
     console.log('hey, props ->', this.props)
+    const {isLoading, article} = this.state
+    if (isLoading) return <Loader />
     return (
       <div>
         <h1>Single article</h1>
