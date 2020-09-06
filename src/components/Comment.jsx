ba474dbc8ed4c378e600127aa6e6ddbb0b623381
“Steve <stevedunnad@gmail.com>
2020-09-06 20:36:45 +0100

    jhard coded user

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import VoteCaster from './VoteCaster'
 import DeleteButton from './DeleteButton'
-import {UserContext} from '../user-context'
 
-const Comment = ({comment, fetchArticleComments}) => {
-  const user = React.useContext(UserContext)
+const Comment = ({ comment, fetchArticleComments }) => {
+  const user = 'cooljmessy'
   return (
-    <article> 
-      user is {user}
+    <article>
       <h3>{comment.author}</h3>
       <h4>{comment.created_at}</h4>
       <p>{comment.body}</p>
-      {user === "cooljmessy" ? (
-      <VoteCaster id={comment.comment_id} votes={comment.votes} type={"comments"} />
+      {user === comment.author ? (
+        <VoteCaster id={comment.comment_id} votes={comment.votes} type={"comments"} />
       ) : (
-        <p>login to vote!</p>
-      )}
-      <DeleteButton comment_id={comment.comment_id} fetchArticleComments={fetchArticleComments}/>
+          <p>login to vote!</p>
+        )}
+      {user === comment.author ?
+        <DeleteButton comment_id={comment.comment_id} fetchArticleComments={fetchArticleComments} /> :
+        <p>you cant delete this</p>}
     </article>
   );
 };
--- src/user-context.js
+++ src/user-context.js
@@ -1,2 +1,22 @@
-import React from 'react'
-export const UserContext = React.createContext('user1')
+// // import React from 'react'
+// // export const UserContext = React.createContext({
+// //   user: 'user1'
+// // })
+
+// import React from "react";
+
+// const Context = React.createContext();
+
+// export class UserProvider extends React.Component {
+//   state = { isADuck: false };
+//   render() {
+//     const { children } = this.props;
+//     return (
+//       <Context.Provider value={this.state}>
+//         {children}
+//       </Context.Provider>
+//     );
+//   }
+// }
+
+// export const UserConsumer = Context.Consumer;
